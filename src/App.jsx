7d0f8c099d81c583724c1fbe0a90ecb87dd6a212
Emya101 <160042834+Emya101@users.noreply.github.com>
2025-09-07 10:20:18 -0500

    Initial vite project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img/>
+        <h2>To-Do App</h2>
+      </header>
+
+      <div>Content</div>
+    </div>
+  )
+}
+
+export default App
