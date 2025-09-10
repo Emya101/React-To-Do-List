e36cb75f061025ed436d8afb1012a480154983cb
Emya101 <160042834+Emya101@users.noreply.github.com>
2025-09-10 12:24:07 -0500

    Made a css fiile for TodoList for added styling

--- src/Components/ToDoForm/TodoList/TodoList.jsx
+++ src/Components/ToDoForm/TodoList/TodoList.jsx
@@ -1,18 +1,25 @@
+import styles from "./TodoList.module.css"
+
 export function TodoList({todos}){
     return(
         <section>
             <h3>To-Do's</h3>
-            <ul>
+            <ul className={styles.TodoList}>
                 {todos.map((todo) => (
-                        <li key={todo.id}>
-                            <input type="checkbox" name="completed" defaultChecked={todo.completed}/>
-                            <div>
-                            {todo.name}<br/>
-                            {todo.description}<br/>
+                        <li key={todo.id} className={styles.TodoListItem} data-completed={todo.completed}>
+                            <div className={styles.Content}>
+                            <input type="checkbox" name="completed" defaultChecked={todo.completed} className={styles.Status}/>
+                            <div className={styles.Info}>
+                            {todo.name}
+                            {todo.description && <span className={styles.Description}>{todo.description}</span>}
+                            <div className={styles.AdditionalInfo}>
+                            {todo.deadline}<br/>
                             {todo.priority !== "none" && todo.priority}<br/>
                             {todo.status !="" && todo.status}<br/>
-                            {todo.category!="" && todo.category} {todo.createdAt}
+                            {todo.category!="" && todo.category}<br/> {todo.createdAt}
+                            </div>
                             </div> 
+                            </div>
                         </li>
                     )
                 )}
